d3641231c277f2f02d45b0947027b7f6e48dfed9
nabijonov05 <m.nabijonov45@gmail.com>
2026-03-14 13:27:55 +0500

    irstrfrefrevfftgg

--- build.sh
+++ build.sh
@@ -1,12 +1,9 @@
-<<<<<<< HEAD
-#!/usr/bin/env bash
-# Xatolik bo'lsa to'xtatish
 set -o errexit
 
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-=======
 
->>>>>>> origin/main
+
+
